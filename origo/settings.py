b53643e01dc3bc2a29bff8a52d2266c9716e9f6d
Frida Merell <frida.merell@gmail.com>
2026-08-24 22:12:31 +0200

    Det sista inför lansering - ser till att den funkar med postgres

--- origo/settings.py
+++ origo/settings.py
@@ -81,12 +81,16 @@ WSGI_APPLICATION = 'origo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
+# DATABASE_URL unset -> local SQLite. Set it (e.g. to a Neon connection
+# string) in production to use Postgres instead.
+
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+    )
 }
 
 
