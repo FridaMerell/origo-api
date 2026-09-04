c8e4dad3cddbaf3c65a7d2f56bda8b6f4401df4b
Frida Merell <frida.merell@gmail.com>
2026-09-04 19:29:02 +0200

    Index på namn

--- tempus/models.py
+++ tempus/models.py
@@ -455,6 +455,7 @@ class Checklist(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    auto_add = models.BooleanField(default=True)
 
     class Meta:
         ordering = ("-created_at",)
